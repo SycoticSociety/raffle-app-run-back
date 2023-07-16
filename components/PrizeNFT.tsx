30a439c3a2af452321ca2031d90f571bdcc692cd
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-07-16 15:04:36 -0400

    Update PrizeNFT.tsx

--- components/PrizeNFT.tsx
+++ components/PrizeNFT.tsx
@@ -3,50 +3,32 @@ import { ThirdwebNftMedia, useContract, useContractMetadata, useContractRead, us
 import { LOTTERY_CONTRACT_ADDRESS } from "../const/addresses";
 
 export default function PrizeNFT() {
-    const {
-        contract: lotteryContract
-    } = useContract(LOTTERY_CONTRACT_ADDRESS);
+    const { contract: lotteryContract } = useContract(LOTTERY_CONTRACT_ADDRESS);
 
-    const {
-        data: nftContractAddress
-    } = useContractRead(lotteryContract, "nftContract");
-    const {
-        data: nftTokenId
-    } = useContractRead(lotteryContract, "tokenId");
+    const { data: nftContractAddress } = useContractRead(lotteryContract, "nftContract");
+    const { data: nftTokenId } = useContractRead(lotteryContract, "tokenId");
 
-    const {
-        contract: nftContract
-    } = useContract(nftContractAddress);
-    const {
-        data: nftContractMetadata,
-        isLoading: nftContractMetadataLoading
-    } = useContractMetadata(nftContract);
+    const { contract: nftContract } = useContract(nftContractAddress);
+    const { data: nftContractMetadata, isLoading: nftContractMetadataLoading } = useContractMetadata(nftContract);
 
-    const {
-        data: nft,
-        isLoading: nftLoading
-    } = useNFT(nftContract, nftTokenId);
+    const { data: nft, isLoading: nftLoading } = useNFT(nftContract, nftTokenId);
 
     return (
-        <Card p={"5%"}>
-                <Heading>Prize NFT</Heading>
-                {!nftContractMetadataLoading && !nftLoading ? (
-                    <Stack spacing={"20px"} textAlign={"center"}>
-                        <Box>
-                            <ThirdwebNftMedia
-                                metadata={nft?.metadata!}
-                                height="100%"
-                                width="100%"
-                            />
-                        </Box>
-                        <Box>
-                            <Text fontSize={"2xl"} fontWeight={"bold"}>{nftContractMetadata.name}</Text>
-                            <Text fontWeight={"bold"}>{nft?.metadata.name}</Text>
-                        </Box>
-                    </Stack>
-                ) : (
-                    <Spinner />
-                )}
+        <Card p="5%" ml="20px" bg="rgba(255, 255, 255, 0.7)">
+            <Heading></Heading>
+            {!nftContractMetadataLoading && !nftLoading ? (
+                <Stack spacing="20px" textAlign="center">
+                    <Box>
+                        <ThirdwebNftMedia metadata={nft?.metadata!} height="100%" width="100%" />
+                    </Box>
+                    <Box>
+                        <Text fontSize="2xl" fontWeight="bold">{nftContractMetadata.name}</Text>
+                        <Text fontWeight="bold">{nft?.metadata.name}</Text>
+                    </Box>
+                </Stack>
+            ) : (
+                <Spinner />
+            )}
         </Card>
-    )
+    );
 }
