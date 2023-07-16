ee59ceae9463b25228f65b9cf4ee5019196329a3
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-07-16 11:36:16 -0400

    Update addresses.ts

--- const/addresses.ts
+++ const/addresses.ts
@@ -1,3 +1,3 @@
-export const LOTTERY_CONTRACT_ADDRESS = '0xc362d21241cA57987ac8517bCc2b34697c5C5266';
+export const LOTTERY_CONTRACT_ADDRESS = '0x742f38fE59c59291592FA68b89Bccf69E0ee470E';
 
 export const HERO_IMAGE_URL = 'ipfs://QmS1gz2fNL6DfnfebNsY3KkdYh3vT4jcEArkndao6eCk5Z/Untitled%20design%20(14).png';
